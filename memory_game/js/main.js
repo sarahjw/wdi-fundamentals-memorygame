5a201d01016fc1a57b746f84a0b59d06232e4acf
arghmatey <40401149+arghmatey@users.noreply.github.com>
2019-11-21 19:38:47 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,7 @@ const cards = [
 	
 const cardsInPlay = [];
 
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -31,9 +32,11 @@ function checkForMatch() {
 		};
 }
 
-function flipCard(cardId) {
+function flipCard() {
+const cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);	
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		 checkForMatch();
 	};
@@ -41,5 +44,16 @@ console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		const cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', [i])
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+createBoard();
+
